cdaf4c8e13a72a3a5b0d3035f1603e910a9797a7
lhty24 <lianghengty@gmail.com>
2019-06-23 23:32:31 -0400

    1.Added rythmbox picture 2.Changed code to show rythembox motion

--- android/src/com/morpheme/palmpiano/PalmPiano.java
+++ android/src/com/morpheme/palmpiano/PalmPiano.java
@@ -5,29 +5,89 @@ import android.content.Context;
 import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.graphics.g2d.Sprite;
-import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.InputListener;
+import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.Touchable;
-import com.badlogic.gdx.scenes.scene2d.utils.ActorGestureListener;
 import com.morpheme.palmpiano.util.Constants;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class PalmPiano implements ApplicationListener {
-    private PianoStage stage;
-    private EventBus eb;
-    private Context context;
+	private Stage stage;
+	private EventBus eb;
+	private Context context;
 
-    public PalmPiano(Context context) {
-    	super();
+	public PalmPiano(Context context) {
+		super();
 		this.context = context;
+	}
+
+	public class RhythmBox extends Actor {
+		boolean gr = false;
+		String note;
+		Texture texture;
+		Sprite sprite;
+		float actorX = 0, actorY = 0;
+		boolean started = true;
+
+		public RhythmBox(boolean gr, final String note, float x){
+			//String file = "box_gr.png";
+			String file = "t1.png";
+			this.gr = gr;
+			if (gr) {
+				this.actorY = 1000;
+//				file = "box_gr.png";
+				file = "t1.png";
+			}
+			texture = new Texture(Gdx.files.internal(file));
+			sprite = new Sprite(texture);
+			this.note = note;
+			this.actorX = x;
+			setBounds(actorX,actorY,texture.getWidth(),texture.getHeight());
+//			addListener(new InputListener(){
+//				public boolean touchDown (InputEvent event, float x, float y, int pointer, int button) {
+////					setBounds(actorX,actorY,texture.getWidth(),texture.getHeight());
+//					pressed = true;
+//					System.out.println(note);
+//					return true;
+//				}
+//
+//				public void touchUp (InputEvent event, float x, float y, int pointer, int button) {
+//					pressed = false;
+//				}
+//			});
+			addListener(new InputListener(){
+				public boolean touchDown (InputEvent event, float x, float y, int pointer, int button) {
+					((RhythmBox)event.getTarget()).started = true;
+					return true;
+				}
+			});
+		}
+
+
+		@Override
+		public void draw(Batch batch, float alpha){
+			// Visual feedback that key is pressed
+			if (this.started)
+				batch.setColor(1,1,1, 0.5f);
+			else
+				batch.setColor(1,1,1, 1);
+			batch.draw(texture,actorX,actorY);
+			batch.setColor(1,1,1,1);
+		}
+
+		@Override
+		public void act(float delta){
+			if(started){
+				actorY-=5;
+			}
+		}
 	}
 
 	public class PianoKey extends Actor {
@@ -55,14 +115,14 @@ public class PalmPiano implements ApplicationListener {
 //					setBounds(actorX,actorY,texture.getWidth(),texture.getHeight());
 					pressed = true;
 					System.out.println(midiNote);
-					eb.dispatch(new Event<Object>(Event.EventType.PIANO_KEY_DOWN, midiNote));
+//					eb.dispatch(new Event<Object>(Event.EventType.PIANO_KEY_DOWN, midiNote));
 					return true;
 				}
 
 				public void touchUp (InputEvent event, float x, float y, int pointer, int button) {
 					pressed = false;
 					System.out.println(midiNote);
-					eb.dispatch(new Event<Object>(Event.EventType.PIANO_KEY_UP, midiNote));
+//					eb.dispatch(new Event<Object>(Event.EventType.PIANO_KEY_UP, midiNote));
 				}
 			});
 		}
@@ -88,25 +148,30 @@ public class PalmPiano implements ApplicationListener {
 	@Override
 	public void create() {
 		eb = EventBus.getInstance();
-		stage = new PianoStage();
+		stage = new Stage();
 		Gdx.input.setInputProcessor(stage);
 
 		List<PianoKey> wks = new ArrayList<>();
 		List<PianoKey> bks = new ArrayList<>();
+		List<RhythmBox> boxes = new ArrayList<RhythmBox>();
 
-		// Order of notes in octave
-		String[] notes = {"A", "AS", "B", "C", "CS", "D", "DS", "E", "F", "FS", "G", "GS"};
+//		PianoKey myActor = new PianoKey();
+
+		// White keys
+		// Index starts from C, D, E, F, G, A, B
+		String[] notes = {"C", "CS", "D", "DS", "E", "F", "FS", "G", "GS", "A", "AS", "B"};
+//		int keyIndex = 2;
 
 		for (int oc = 0; oc < 7; oc++) {
-			int offset = oc * (7 * (Constants.WK_WIDTH + Constants.WK_GAP));
+			int offset = 0 + oc * (7 * (Constants.WK_WIDTH + Constants.WK_GAP));
 			boolean bk;
 			for (int i = 0; i < notes.length; i++) {
-				if ( i == 1 || i == 4|| i == 6 || i == 9 || i == 11 ) {
+				if ( (i < 5 && i % 2 == 1) || (i >= 5 && i % 2 == 0) ) {
 					bk = true;
 				} else {
 					bk = false;
 				}
-				PianoKey k = new PianoKey(bk, (byte) (Constants.MIDI_OFFSET + i + oc*12),  bk ? offset - Constants.BK_WIDTH/2 : offset);
+				PianoKey k = new PianoKey(bk, (byte) (i + oc*12),  bk ? offset - Constants.BK_WIDTH/2 : offset);
 				k.setTouchable(Touchable.enabled);
 				if (bk) {
 					bks.add(k);
@@ -125,6 +190,34 @@ public class PalmPiano implements ApplicationListener {
 		for (PianoKey bk : bks) {
 			stage.addActor(bk);
 		}
+
+		// Rhythm Boxes
+		for(int i = 0; i < 18; i++) {
+			RhythmBox box = new RhythmBox(true, "w"+i, i*(130));
+			boxes.add(box);
+			box.setTouchable(Touchable.enabled);
+			stage.addActor(box);
+		}
+
+//		// Black keys
+//		// Refactor checking into exclusion set
+//		// Note ES and BS are never used, refactor later
+//		String[] notesSharp = {"CS", "DS", "ES", "FS", "GS", "AS", "BS"};
+//		keyIndex = 2;
+//		octave = 3;
+//		for(int i = 0; i < 18; i++) {
+//			keyIndex++;
+//			if (keyIndex % 7 == 0) {
+//				keyIndex = 0;
+//				octave++;
+//			}
+//			if ( i == 3 || i == 6 || i == 10 || i == 13 || i == 17 )
+//				continue;
+//			PianoKey bk = new PianoKey(true, SoundPlayer.Note.valueOf(notesSharp[keyIndex]+octave), (Constants.WK_WIDTH + Constants.WK_GAP)-Constants.BK_WIDTH/2 + i*(Constants.WK_WIDTH + Constants.WK_GAP));
+//			bks.add(bk);
+//			bk.setTouchable(Touchable.enabled);
+//			stage.addActor(bk);
+//		}
 
 		SoundPlayer.initialize(context);
 	}
