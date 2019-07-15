d74bae6cb2fe127ffc50e5c9d8523fe69ba5b569
arcyleung <arcyleung@gmail.com>
2019-07-15 03:07:50 -0400

    Clean up comments add qualifiers

--- android/src/com/morpheme/palmpiano/PalmPiano.java
+++ android/src/com/morpheme/palmpiano/PalmPiano.java
@@ -59,15 +59,14 @@ public class PalmPiano implements ApplicationListener {
 	}
 
 	public static class RhythmBox extends Actor {
-		boolean bk = true;
-		//String note;
-		public static Texture textureWk;
-		public static Texture textureBk;
+		private boolean bk = true;
+		private static Texture textureWk;
+		private static Texture textureBk;
 
 		private long duration;
 
-		float actorX = 0, actorY = 0;
-		boolean started = true;
+		private float actorX = 0, actorY = 0;
+		private boolean started = true;
 
 		public RhythmBox(boolean bk, int midi_note, long duration){
 			this.bk = bk;
@@ -112,12 +111,12 @@ public class PalmPiano implements ApplicationListener {
 	public enum PianoMode {MODE_COMPOSITION, MODE_PLAYBACK, MODE_GAME}
 
 	public class PianoKey extends Actor {
-		boolean bk = false;
-		Byte midiNote = 0x0b;
-		Texture texture;
-		Sprite sprite;
-		float actorX = 0, actorY = 0;
-		boolean pressed;
+		private boolean bk = false;
+		private Byte midiNote = 0x0b;
+		private Texture texture;
+		private Sprite sprite;
+		private float actorX = 0, actorY = 0;
+		private boolean pressed;
 
 		public PianoKey(boolean bk, final Byte midiNote, float x){
 			String file = "wk.png";
@@ -212,77 +211,6 @@ public class PalmPiano implements ApplicationListener {
 			stage.addActor(bk);
 		}
 
-//		SoundPlayer.initialize(context);
-
-		//Rhythm Boxes
-		//if (this.mode == PianoMode.MODE_GAME) {
-//			for (int oc = 0; oc < 7; oc++) {
-//				//int offset = 0 + oc * (7 * (Constants.WK_WIDTH + Constants.WK_GAP));
-//				boolean bk;
-//				for (int i = 0; i < notes.length; i++) {
-//					if ((i < 5 && i % 2 == 1) || (i >= 5 && i % 2 == 0)) {
-//						bk = true;
-//					} else {
-//						bk = false;
-//					}
-//
-//					//RhythmBox box = new RhythmBox(bk, bk ? offset - Constants.BK_WIDTH/2 : offset);
-//					//RhythmBox box = new RhythmBox(true, bk? i*(130)+30-45 : i*(130)+30);
-//					//RhythmBox box = new RhythmBox(true, bk ? i*(130) : i*(130) - 45 );
-//					RhythmBox box = new RhythmBox(true, i * (130));
-//					box.setTouchable(Touchable.enabled);
-//					boxes.add(box);
-////				if (bk) {
-////					boxes_bk.add(box);
-////					continue;
-////				} else {
-////					boxes_wk.add(box);
-////				}
-//					//offset += (Constants.WK_WIDTH + Constants.WK_GAP);
-//					stage.addActor(box);
-//				}
-//			}
-		//}
-
-//		for(int i = 0; i < 18; i++) {
-//			RhythmBox box = new RhythmBox(true,i*(130));
-//			boxes_bk.add(box);
-//			box.setTouchable(Touchable.enabled);
-//			stage.addActor(box);
-//		}
-
-//		// Black keys
-//		// Refactor checking into exclusion set
-//		// Note ES and BS are never used, refactor later
-//		String[] notesSharp = {"CS", "DS", "ES", "FS", "GS", "AS", "BS"};
-//		keyIndex = 2;
-//		octave = 3;
-//		for(int i = 0; i < 18; i++) {
-//			keyIndex++;
-//			if (keyIndex % 7 == 0) {
-//				keyIndex = 0;
-//				octave++;
-//			}
-//			if ( i == 3 || i == 6 || i == 10 || i == 13 || i == 17 )
-//				continue;
-//			PianoKey bk = new PianoKey(true, SoundPlayer.Note.valueOf(notesSharp[keyIndex]+octave), (Constants.WK_WIDTH + Constants.WK_GAP)-Constants.BK_WIDTH/2 + i*(Constants.WK_WIDTH + Constants.WK_GAP));
-//			bks.add(bk);
-//			bk.setTouchable(Touchable.enabled);
-//			stage.addActor(bk);
-//		}
-
-//		TextButton.TextButtonStyle buttonBackStyle = new TextButton.TextButtonStyle();
-//		buttonBackStyle.font = new BitmapFont();
-//		TextureAtlas buttonBackAtlas = new TextureAtlas(Gdx.files.internal("buttons/buttons.pack"));
-//		Skin skin = new Skin();
-//		skin.addRegions(buttonBackAtlas);
-//		buttonBackStyle.font = new BitmapFont();
-//		buttonBackStyle.up = skin.getDrawable("up-button");
-//		buttonBackStyle.down = skin.getDrawable("down-button");
-//		buttonBackStyle.checked = skin.getDrawable("checked-button");
-//		buttonBack = new TextButton("Go Back", buttonBackStyle);
-//		stage.addActor(buttonBack);
-
 		Intent intent = ((Activity) context).getIntent();
 		Bundle bundle = intent.getExtras();
 		if (bundle != null) {
@@ -299,18 +227,6 @@ public class PalmPiano implements ApplicationListener {
 					for (int oc = 0; oc < 7; oc++) {
 						//int offset = 0 + oc * (7 * (Constants.WK_WIDTH + Constants.WK_GAP));
 						boolean bk;
-//						for (int i = 0; i < notes.length; i++) {
-//							if ((i < 5 && i % 2 == 1) || (i >= 5 && i % 2 == 0)) {
-//								bk = true;
-//							} else {
-//								bk = false;
-//							}
-//
-//							RhythmBox box = new RhythmBox(true, i * (130));
-//							box.setTouchable(Touchable.enabled);
-//							boxes.add(box);
-//							stage.addActor(box);
-//						}
 
 						//RhythmBox box = new RhythmBox(true, getNotePosition((byte) 80));
 						RhythmBox box = new RhythmBox(false, 21, 1);
