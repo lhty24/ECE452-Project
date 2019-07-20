9a4e8c4c3b637cb1af625204de0a5e519756208b
Michael Li <limichael97@gmail.com>
2019-07-20 18:02:06 -0400

    Fix extended sound bug on pause

--- android/src/com/morpheme/palmpiano/SoundPlayer.java
+++ android/src/com/morpheme/palmpiano/SoundPlayer.java
@@ -2,6 +2,7 @@ package com.morpheme.palmpiano;
 
 import com.morpheme.palmpiano.util.Constants;
 
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -13,13 +14,22 @@ public class SoundPlayer implements EventListener {
     private HashSet<Event.EventType> monitoredEvents;
     private MidiDriver midi;
 
+    private ArrayList<Byte> notes = new ArrayList<>();
+    private boolean paused;
+
     /* Constructor stuff */
     private SoundPlayer() {
         super();
+
         this.monitoredEvents = new HashSet<>();
-        monitoredEvents.add(Event.EventType.PIANO_KEY_DOWN);
-        monitoredEvents.add(Event.EventType.PIANO_KEY_UP);
-        monitoredEvents.add(Event.EventType.MIDI_DATA_AUDIO);
+        this.monitoredEvents.add(Event.EventType.PIANO_KEY_DOWN);
+        this.monitoredEvents.add(Event.EventType.PIANO_KEY_UP);
+        this.monitoredEvents.add(Event.EventType.MIDI_DATA_AUDIO);
+        this.monitoredEvents.add(Event.EventType.MIDI_FILE_PAUSE);
+        this.monitoredEvents.add(Event.EventType.MIDI_FILE_PLAY);
+        this.monitoredEvents.add(Event.EventType.BACK);
+        this.monitoredEvents.add(Event.EventType.PAUSE);
+        this.monitoredEvents.add(Event.EventType.RESUME);
 
         midi = new MidiDriver();
         midi.start();
@@ -32,9 +42,7 @@ public class SoundPlayer implements EventListener {
     }
 
     public static SoundPlayer getInstance() {
-        if (soundPlayer == null) {
-            soundPlayer = new SoundPlayer();
-        }
+        if (soundPlayer == null) soundPlayer = new SoundPlayer();
         return soundPlayer;
     }
 
@@ -43,12 +51,29 @@ public class SoundPlayer implements EventListener {
         switch (event.getEventType()) {
             case PIANO_KEY_DOWN:
                 this.playNote((Byte) event.getData());
+                this.notes.add((Byte) event.getData());
                 break;
             case PIANO_KEY_UP:
                 this.stopNote((Byte) event.getData());
+                this.notes.remove((Byte) event.getData());
                 break;
             case MIDI_DATA_AUDIO:
                 this.playMidi((byte[]) event.getData());
+                break;
+            case BACK:
+            case PAUSE:
+                this.pauseNotes();
+                break;
+            case MIDI_FILE_PAUSE:
+                this.paused = true;
+                this.pauseNotes();
+                break;
+            case RESUME:
+                this.resumeNotes();
+                break;
+            case MIDI_FILE_PLAY:
+                this.paused = false;
+                this.resumeNotes();
                 break;
             default:
                 break;
@@ -70,14 +95,12 @@ public class SoundPlayer implements EventListener {
         sendMidiNote(false, 1, note, Constants.MIDI_PIANO_MEZZO_FORTE);
     }
 
-    private void sendMidiNote(boolean noteOn, int channel, int note, int volume)
-    {
+    private void sendMidiNote(boolean noteOn, int channel, int note, int volume) {
         byte message[] = new byte[3];
 
         byte midiMessage = (byte) (Constants.MIDI_NOTE_OFF | channel);
-        if (noteOn) {
-            midiMessage |= Constants.MIDI_NOTE_ON;
-        }
+
+        if (noteOn) midiMessage |= Constants.MIDI_NOTE_ON;
 
         message[0] = midiMessage;
         message[1] = (byte) note;
@@ -88,7 +111,22 @@ public class SoundPlayer implements EventListener {
 
     private void playMidi(byte[] midiMessage) {
         midi.write(midiMessage);
+
+        boolean on = (midiMessage[0] ^ Constants.MIDI_NOTE_ON) - 1 == 0;
+
+        if (on) {
+            notes.add(midiMessage[1]);
+        } else {
+            notes.remove((Byte) midiMessage[1]);
+        }
+    }
+
+    private void pauseNotes() {
+        for (byte note : notes) stopNote(note);
+    }
+
+    private void resumeNotes() {
+        if (paused) return;
+        for (byte note : notes) playNote(note);
     }
 }
-
-// On pause, stop all sounds or else you will hear a long note.
